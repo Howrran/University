1b7e0f61b58ca9a3cd5ac052fe802df032a2d797
Mykola Matviiv <infiniti943@gmail.com>
2019-04-23 22:21:21 +0300

    add Lab 3

---
+++ DataBase/Lab_3/SQLQuery_Lab_3.sql
@@ -0,0 +1,14 @@
+use Library_test;
+
+Alter table Test_3
+	drop FK_Test1_Test3
+
+
+--exec sp_rename 'Test_3.qwerty','new', 'column'
+
+--	add constraint FK_TEST_1 foreign key(test_1_id) references Test_1(id)
+--  add Primary key(id)
+--  alter column id int not null
+--	add test_column int, test_column2 int, test_column3 nvarchar(50) 
+--	drop column test_column, test_column2, test_column3
+
