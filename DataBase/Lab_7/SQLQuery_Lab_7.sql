a2968aff86f1095a96684ebeb05b3207beec8d2a
Mykola Matviiv <infiniti943@gmail.com>
2019-05-09 23:27:52 +0300

    add Lab 7

---
+++ DataBase/Lab_7/SQLQuery_Lab_7.sql
@@ -0,0 +1,15 @@
+use Library;
+
+
+--select * from  Book_Catalog where book_id_catalog between 10 and 25
+--Order By book_id_catalog DESC
+
+--select * from [User] where phone_number like '%21%'
+
+--select * from Book_Catalog inner join Book_Borrow on book_id_catalog = Book_Borrow.book_id
+
+--select book_id, user_id, borrow_date, period, id, name, surname, phone_number, adress
+--from Book_Borrow right join [User] on user_id = id
+
+--select * from Publisher left join Book on Publisher.id = Book.publisher_id
+
